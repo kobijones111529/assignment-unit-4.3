afbc0672fe4d020f40b754e6e3324b9b793654a0
Kobi Jones <kobijonesmail@gmail.com>
2023-03-16 22:54:56 -0500

    Expand equal function to work with all types

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,33 +3,66 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 /**
- * Utility function for comparing arrays
- * @param {any[]} a First array
- * @param {any[]} b Second array
+ * Utility function for doing a strict
+ * deep comparison of two values of
+ * any type
+ * @param {any[]} a First value
+ * @param {any[]} b Second value
  * @returns 
  */
 function equal(a, b) {
-  if (a.length != b.length)
+  if (typeof a !== typeof b)
     return false;
-  
-  for (const i in a) {
-    if (a[i] !== b[i])
-      return false;
+
+  // typeof null === 'object'
+  if (a === null && b === null)
+    return true;
+  else if (
+    a === null && b !== null ||
+    b === null && a !== null
+  )
+    return false;
+
+  if (typeof a === 'object') {
+    if (a instanceof Array) {
+      if (a.length !== b.length)
+        return false;
+
+      for (const i in a) {
+        if (!equal(a[i], b[i]))
+          return false;
+      }
+
+      return true;
+    }
+
+    // Sort and compare defined entries
+    const definedEntries = obj => Object.entries(obj).filter(([_, value]) => value !== undefined);
+    return equal(definedEntries(a).sort(), definedEntries(b).sort());
   }
 
-  return true;
+  return a === b;
 }
+
+console.assert(equal(undefined, undefined), '%o should equal %o', undefined, undefined);
+console.assert(equal(null, null), '%o should equal %o', null, null);
+console.assert(!equal(null, { a: 1 }), '%o should not equal %o', null, { a: 1 });
+console.assert(!equal({}, null), '%o should not equal %o', {}, null);
+console.assert(equal({ a: undefined }, {}), '%o should equal %o', { a: undefined }, {});
+console.assert(equal({ a: 1, b: 2 }, { b: 2, a: 1 }), '%o should equal %o', { a: 1, b: 2 }, { b: 2, a: 1 });
+console.assert(!equal(undefined, false), '%o should not equal %o', undefined, false);
+console.assert(equal({ a: [1, 2], b: 'hi' }, { a: [1, 2], b: 'hi' }), '%o should equal %o', { a: [1, 2], b: 'hi' }, { a: [1, 2], b: 'hi' });
+console.assert(equal([], []), '%o should equal %o', [], []);
+console.assert(!equal([], [1, 2]), '%o should not equal %o', [], [1, 2]);
+console.assert(equal(['hello', 'there'], ['hello', 'there']), '%o should equal %o', ['hello', 'there'], ['hello', 'there']);
+console.assert(!equal([1, 2, 3], [1, 3, 2]), '%o should not equal %o', [1, 2, 3], [1, 3, 2]);
+console.assert(equal([[1, 2], ['hi', 'hello']], [[1, 2], ['hi', 'hello']]), '%o should equal %o', [[1, 2], ['hi', 'hello']], [[1, 2], ['hi', 'hello']]);
 
 const testHeaderCSS = `
   font-family: "Lucida Console", monospace;
   font-weight: lighter;
   font-size: 1.2em;
 `;
-
-console.assert(equal([], []), '%o should equal %o', [], []);
-console.assert(!equal([], [1, 2]), '%o should not equal %o', [], [1, 2]);
-console.assert(equal(['hello', 'there'], ['hello', 'there']), '%o should equal %o', ['hello', 'there'], ['hello', 'there'])
-console.assert(!equal([1, 2, 3], [1, 3, 2]), '%o should not equal %o', [1, 2, 3], [1, 3, 2]);
 
 const maxItems = 5;
 let basket = [];
@@ -67,7 +100,7 @@ function removeItem(item) {
 
   if (index === -1)
     return null;
-  
+
   return basket.splice(index, 1)[0];
 }
 
@@ -79,7 +112,7 @@ function removeItem(item) {
   basket = [];
   console.log('Basket is', basket);
   const newItem = 'apple';
-  console.log('Adding %o to basket', newItem)
+  console.log('Adding %o to basket', newItem);
   console.log('addItem (should be %o):', !isFull(), addItem(newItem));
   console.assert(equal(basket, [newItem]), 'Basket should be', [newItem]);
   console.log('Basket is', basket);
